a7abbdae4d56cefdc92d13b491c4ad14accc731e
Zeeger <zeeger@lubsen.eu>
2021-11-09 21:48:38 +0100

    Some fiddling

--- src/gui/gui.rs
+++ src/gui/gui.rs
@@ -1,5 +1,6 @@
 use std::thread::{Builder, JoinHandle};
-use std::io::{stdout, Error};
+use std::io::{stdout, Error, Stdout};
+use std::sync::Arc;
 use std::time::Duration;
 
 use tokio::sync::broadcast::{Sender, Receiver};
@@ -31,6 +32,7 @@ struct App {
     active_area : Area,
     prompt : String,
     kill_signal : bool,
+    selected_index : usize,
     command_tx : Sender<Command>,
 }
 
@@ -80,115 +82,123 @@ impl App {
     }
 }
 
-pub fn start_gui(mut command_rx: Receiver<Command>, command_tx : Sender<Command>, mut event_rx: Receiver<Event>) -> Result<JoinHandle<()>, Error> {
+pub fn start_gui(mut command_rx: Receiver<Command>, command_tx : Sender<Command>,
+                 mut event_rx: Receiver<Event>) -> Result<JoinHandle<()>, Error> {
     let mut app = App {
         active_area : Area::Prompt,
         prompt : String::default(),
         kill_signal : false,
+        selected_index: 0,
         command_tx
     };
 
-    let gui_config = Config::current();
+    let config = Config::current();
     let gui_builder = Builder::new().name("GUI".into());
     gui_builder.spawn(move || {
         let stdout = stdout();
         let backend = CrosstermBackend::new(stdout);
-        let mut terminal = Terminal::new(backend).unwrap();
-        terminal.clear().unwrap();
+        let mut terminal = Terminal::new(backend).expect("Failed to create terminal.");
+        terminal.clear().expect("Failed to clear terminal.");
 
-        // let menu_titles = vec!["Stats", "Routes"];
+        let mut terminal = gui_loop(config, app, terminal, command_rx, event_rx);
 
-        while !app.kill_signal {
-            terminal.draw(|rect| {
-                let size = rect.size();
-                let chunks = Layout::default()
-                    .direction(Direction::Vertical)
-                    .margin(2)
-                    .constraints(
-                        [
-                            Constraint::Length(3),
-                            Constraint::Min(2),
-                            Constraint::Length(6),
-                        ]
-                            .as_ref(),
-                    )
-                    .split(size);
-
-                let prompt = Paragraph::new(format!("{}{}", PROMPT_START, app.prompt))
-                    .style(Style::default().fg(Color::LightGreen))
-                    .alignment(Alignment::Left)
-                    .block(
-                        Block::default()
-                            .borders(Borders::ALL)
-                            .style(Style::default().fg(Color::White))
-                            .title("Console:")
-                            .border_type(BorderType::Plain),
-                    );
-                rect.render_widget(prompt, chunks[2]);
-
-                // let menu = menu_titles
-                //     .iter()
-                //     .map(|t| {
-                //         let (first, rest) = t.split_at(1);
-                //         Spans::from(vec![
-                //             Span::styled(
-                //                 first,
-                //                 Style::default()
-                //                     .fg(Color::Yellow)
-                //                     .add_modifier(Modifier::UNDERLINED),
-                //             ),
-                //             Span::styled(rest, Style::default().fg(Color::White)),
-                //         ])
-                //     })
-                //     .collect();
-
-                // let tabs = Tabs::new(menu)
-                //     .select(0)
-                //     .block(Block::default().title("Menu").borders(Borders::ALL))
-                //     .style(Style::default().fg(Color::White))
-                //     .highlight_style(Style::default().fg(Color::Yellow))
-                //     .divider(Span::raw("|"));
-                // rect.render_widget(tabs, chunks[0]);
-
-                let header = Paragraph::new("Simulation Gateway - (c) Zeeger Lubsen, 2021")
-                    .style(Style::default().fg(Color::White))
-                    .block(Block::default().borders(Borders::ALL));
-                rect.render_widget(header, chunks[0]);
-
-                let middle_chunks = Layout::default()
-                    .direction(Direction::Horizontal)
-                    .constraints([Constraint::Ratio(1, 3), Constraint::Ratio(2, 3)].as_ref())
-                    .split(Rect {
-                        x: 0,
-                        y: 0,
-                        width: 9,
-                        height: 2,
-                    });
-
-                let routes = [ListItem::new("Route One"), ListItem::new("Route Two"), ListItem::new("Route Three")]; //settings.routes.iter().map(|r|->ListItem::new(r));
-                let route_list = List::new(routes)
-                    .block(Block::default().title("List").borders(Borders::ALL))
-                    .style(Style::default().fg(Color::White))
-                    .highlight_style(Style::default().add_modifier(Modifier::ITALIC));
-                // rect.render_widget(middle_chunks, chunks[1]);
-                // rect.render_widget(route_list, middle_chunks[1]);
-                // rect.render_widget(middle_chunks, chunks[1]);
-            }).unwrap();
-
-            // handle inputs from other threads
-            // TODO handle refresh rate with a sleep or so.
-            if let Ok(command) = command_rx.try_recv() {
-                app.handle_command(command);
-            }
-
-            // handle incoming data
-            match event_rx.try_recv() {
-                Ok(event) => { debug!("gui received event: {:?}", event) }
-                Err(err) => { debug!("gui data_rx.try_recv() gave an error {:?}", err)}
-            }
-        }
-        disable_raw_mode().unwrap();
-        terminal.show_cursor().unwrap();
-        terminal.clear().unwrap();
+        disable_raw_mode().expect("Failed to disable raw mode.");
+        terminal.show_cursor().expect("Terminal failed to show cursor.");
+        terminal.clear().expect("Failed to clear terminal.");
     })
+}
+
+fn gui_loop(config: Arc<Config>, mut app: App, mut terminal: Terminal<CrosstermBackend<Stdout>>,
+            mut command_rx: Receiver<Command>,
+            mut event_rx: Receiver<Event>) -> Terminal<CrosstermBackend<Stdout>> {
+    while !app.kill_signal {
+        terminal.draw(|rect| {
+            let size = rect.size();
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(2)
+                .constraints(
+                    [
+                        Constraint::Length(3),
+                        Constraint::Min(2),
+                        Constraint::Length(6),
+                    ]
+                        .as_ref(),
+                )
+                .split(size);
+
+            let prompt = Paragraph::new(format!("{}{}", PROMPT_START, app.prompt))
+                .style(Style::default().fg(Color::LightGreen))
+                .alignment(Alignment::Left)
+                .block(
+                    Block::default()
+                        .borders(Borders::ALL)
+                        .style(Style::default().fg(Color::White))
+                        .title("Console:")
+                        .border_type(BorderType::Plain),
+                );
+            rect.render_widget(prompt, chunks[2]);
+
+            // let menu = config.routes
+            //     .iter()
+            //     .map(|r| {
+            //         let (first, rest) = r.split_at(1);
+            //         Spans::from(vec![
+            //             Span::styled(
+            //                 first,
+            //                 Style::default()
+            //                     .fg(Color::Yellow)
+            //                     .add_modifier(Modifier::UNDERLINED),
+            //             ),
+            //             Span::styled(rest, Style::default().fg(Color::White)),
+            //         ])
+            //     })
+            //     .collect();
+
+            // let tabs = Tabs::new(menu)
+            //     .select(0)
+            //     .block(Block::default().title("Menu").borders(Borders::ALL))
+            //     .style(Style::default().fg(Color::White))
+            //     .highlight_style(Style::default().fg(Color::Yellow))
+            //     .divider(Span::raw("|"));
+            // rect.render_widget(tabs, chunks[0]);
+
+            let header = Paragraph::new("Simulation Gateway - (c) 2021, Zeeger Lubsen")
+                .style(Style::default().fg(Color::White))
+                .block(Block::default().borders(Borders::ALL));
+            rect.render_widget(header, chunks[0]);
+
+            let middle_chunks = Layout::default()
+                .direction(Direction::Horizontal)
+                .constraints([Constraint::Ratio(1, 3), Constraint::Ratio(2, 3)].as_ref())
+                .split(Rect {
+                    x: 0,
+                    y: 0,
+                    width: 9,
+                    height: 2,
+                });
+
+            let routes = config.routes.iter().map(|r|ListItem::new(r.name.clone()));
+            // let route_list = List::new(routes)
+            //     .block(Block::default().title("Routes").borders(Borders::ALL))
+            //     .style(Style::default().fg(Color::White))
+            //     .highlight_style(Style::default().add_modifier(Modifier::ITALIC));
+            // rect.render_widget(middle_chunks, chunks[1]);
+            // rect.render_widget(route_list, middle_chunks[1]);
+            // rect.render_widget(middle_chunks, chunks[1]);
+        }).unwrap();
+
+        // handle inputs from other threads
+        // TODO handle refresh rate with a sleep or so.
+        if let Ok(command) = command_rx.try_recv() {
+            app.handle_command(command);
+        }
+
+        // handle incoming data
+        match event_rx.try_recv() {
+            Ok(event) => { debug!("gui received event: {:?}", event) }
+            Err(err) => { debug!("gui data_rx.try_recv() gave an error {:?}", err)}
+        }
+    }
+    terminal
 }
